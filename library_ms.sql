bbffaf51176e82354936581642e905c6c4b32906
T S Weerathunga <weerathungatharusha2004@gmail.com>
2025-01-10 23:49:32 +0530

    Add files via upload

---
+++ library_ms.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 10, 2025 at 06:58 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library_ms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_details`
+--
+
+CREATE TABLE `book_details` (
+  `book_id` int(11) NOT NULL,
+  `book_name` varchar(250) DEFAULT NULL,
+  `author` varchar(100) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `book_details`
+--
+
+INSERT INTO `book_details` (`book_id`, `book_name`, `author`, `quantity`) VALUES
+(1, 'JAVA', 'AUTHOR NAME', 5),
+(2, 'PYTHON', 'AUTHOR NAME', 2),
+(3, 'PSP', 'ABS', 2),
+(4, 'AVB', 'DER', 8),
+(5, 'JKAL', 'MKS', 5),
+(6, 'KJBDS', 'DJKS', 3),
+(7, 'DG', 'HJHJ', 4),
+(8, 'HJGKJ', 'BKJ', 5),
+(9, 'HKKL', 'JKJLH/', 6),
+(10, 'KHGJ', 'LIBKJ', 5),
+(11, 'KILO', 'SSGD', 7),
+(12, 'ESRHFJGK', 'GHJCJHJ', 6),
+(13, 'JHM', 'CTDH', 4),
+(14, 'KGJ', 'TFDG', 8),
+(15, 'JJEKF', 'VFSDKB', 3),
+(16, 'JKAL', 'MKS', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_details`
+--
+
+CREATE TABLE `student_details` (
+  `student_id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `course` varchar(100) DEFAULT NULL,
+  `branch` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_details`
+--
+
+INSERT INTO `student_details` (`student_id`, `name`, `course`, `branch`) VALUES
+(1, 'Naveen', 'DSE', 'Colombo'),
+(2, 'TYFJH', 'DCSD', 'Kurunagala'),
+(3, 'JHF', 'DSE ', 'Colombo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `contact` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `contact`) VALUES
+(1, 'Tony', '123456', 'tony@gmail.com', '0987654321');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_details`
+--
+ALTER TABLE `book_details`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `student_details`
+--
+ALTER TABLE `student_details`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_details`
+--
+ALTER TABLE `book_details`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `student_details`
+--
+ALTER TABLE `student_details`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
